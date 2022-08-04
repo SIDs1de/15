24c9bf2069234950ba083a63ff1b52ee8c5b4586
SIDs1de <igorekdenisenkov@yandex.ru>
2022-08-04 18:53:48 +0300

    добавил ютуб видео

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const heads = document.querySelectorAll('.questions__head');
   const bodies = document.querySelectorAll('.questions__body');
   const menuLinks = document.querySelectorAll('[data-goto]');
+  const popupOpenBtns = document.querySelectorAll('[data-open-pop]');
+  const body = document.querySelector('.page');
+  const popups = document.querySelectorAll('[data-pop]');
+  const popupBgs = document.querySelectorAll('.popup__bg');
+  const html = document.querySelector('html');
 
   const swiper = () => {
     const windowWidth = document.documentElement.clientWidth;
@@ -22,7 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
         },
       });
     } else {
-      console.log('sdfsd');
       const mySwiper = new Swiper('.about__swiper', {
         spaceBetween: 20,
         slidesPerView: 1,
@@ -56,14 +60,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const navigation = () => {
     menuLinks.forEach((menuLink) => {
       menuLink.addEventListener('click', (e) => {
-        let gotoBlock = document.querySelector(menuLink.dataset.goto);
-        let gotoBlockValue =
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue =
           gotoBlock.getBoundingClientRect().top + pageYOffset - 50;
         window.scrollTo({
           top: gotoBlockValue,
           behavior: 'smooth',
         });
         e.preventDefault();
+      });
+    });
+  };
+
+  const openPopup = () => {
+    popupOpenBtns.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        html.classList.add('lock');
+        popups.forEach((popup) => {
+          if (popup.dataset.pop == e.target.dataset.openPop) {
+            popup.classList.add('active');
+          }
+        });
+        e.preventDefault();
+      });
+    });
+    popupBgs.forEach((bg) => {
+      bg.addEventListener('click', (e) => {
+        html.classList.remove('lock');
+        let pop = document.querySelector(
+          `[data-pop="${e.target.dataset.close}"]`
+        );
+        pop.classList.remove('active');
+      });
+    });
+    document.querySelectorAll('[data-close]').forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        let pop = document.querySelector(
+          `[data-pop="${e.target.dataset.close}"]`
+        );
+        pop.classList.remove('active');
+        html.classList.remove('lock');
       });
     });
   };
@@ -75,4 +111,5 @@ document.addEventListener('DOMContentLoaded', () => {
   animations();
   accordion();
   navigation();
+  openPopup();
 });
